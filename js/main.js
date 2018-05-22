54c44d5a1b64208c11a0b0916418d4324bd462aa
coal1633 <32506489+coal1633@users.noreply.github.com>
2018-05-22 17:24:59 +0200

    ajax v1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
+var ok=0;
+var j;
 $(document).ready(function() {
-
+	
 	// show/hide stuff based on if user is doc or not
 	$("#iam").change( function() {
 		var val = $(this).find("option:selected").attr("value");
@@ -9,134 +11,219 @@ $(document).ready(function() {
 		if ( val == "doc" ) {
 			$(".doctor").addClass("show");
 			$(".nurse input").removeAttr('required');
+			$("#SV").attr('required','required');
+			$("select").closest("label").removeClass("error");
 		} else if (val == "nur") {
 			$(".nurse").addClass("show");
 			$(".nurse input").attr('required','required');
+			$("select").closest("label").removeClass("error");
 		}
 	} );
 
 	function goToSection(i){
-  $("fieldset:gt("+i+")").removeClass("current").addClass("next");
-  $("fieldset:lt("+i+")").removeClass("current");
-  $("li:lt("+i+")").removeClass("bar-current").addClass("bar-done");
-  $("li").eq(i).addClass("bar-current").siblings().removeClass("bar-current");
-  $("fieldset").eq(i).removeClass("next").addClass("current active");
-      if ($("fieldset.current").index() == 1 ){
-      	$(".back-btn").addClass("visible");
+  		$("fieldset:gt("+i+")").removeClass("current").addClass("next");
+  		$("fieldset:lt("+i+")").removeClass("current");
+  		$("li:lt("+i+")").removeClass("bar-current").addClass("bar-done");
+  		$("li").eq(i).addClass("bar-current").siblings().removeClass("bar-current");
+  		$("fieldset").eq(i).removeClass("next").addClass("current active");
+      	if ($("fieldset.current").index() == 1 ){
+      		$(".back-btn").addClass("visible");
       	} else if ($("fieldset.current").index() == 2) {
-      	$(".back-btn").removeClass("visible");
-        $("input[type='submit']").val('OK');
-        $("li").addClass("bar-done");
+      		$(".back-btn").removeClass("visible");
+        	$("input[type='submit']").val('OK');
+        	$("li").addClass("bar-done");
       	} else {
       		$(".back-btn").removeClass("visible");
       	}
+	}
  
-}
- 
-function nextSection(){
-  var i = $("fieldset.current").index();
-  if (i < 2){
-    $("li").eq(i+1).addClass("active");
-    goToSection(i+1);
-  }
-}
+	function nextSection(){
+  		var i = $("fieldset.current").index();
+  		if (i < 2){
+    		$("li").eq(i+1).addClass("active");
+    		goToSection(i+1);
+  		}
+	}
 
-$(".back-btn").on("click", function(e){
-  var i = $("fieldset.current").index();
-  $("fieldset").eq(-1).addClass("current");
-  goToSection(i-1);
-});
+	$(".back-btn").on("click", function(e){
+  		var i = $("fieldset.current").index();
+  		$("fieldset").eq(-1).addClass("current");
+  		goToSection(i-1);
+	});
 
-$(".radio").on("click", function(e){
- 	$("#SV").prop('disabled', false); 
- 	$(".cw").prop('disabled', true);
- 	$("#SV").attr('required','required'); 
- 	$(".cw").removeAttr('required');
- 	$(".cw").closest("label").addClass("error");
+	$(".radio").on("click", function(e){
+ 		$("#SV").prop('disabled', false); 
+ 		$(".cw").prop('disabled', true);
+ 		$("#SV").attr('required','required'); 
+ 		$(".cw").removeAttr('required');
+ 		$(".cw").closest("label").removeClass("error");
+	});
 
- 	
-});
+	$(".radio2").on("click", function(e){
+	 	$("#SV").prop('disabled', true); 
+	 	$(".cw").prop('disabled', false);
+	 	$("#SV").removeAttr('required');
+	 	$(".cw").attr('required','required');
+	 	$("#SV").closest("label").removeClass("error");
+	});
 
-$(".radio2").on("click", function(e){
- 	$("#SV").prop('disabled', true); 
- 	$(".cw").prop('disabled', false);
- 	$("#SV").removeAttr('required');
- 	$(".cw").attr('required','required');
-});
+	$("input").blur(function() {
+		var field = $(this);
 
-	// form validation
+		if ( validateField(field) ) {
+			field.closest("label").removeClass("error not-exist exists");
+		} else {
+			field.closest("label").removeClass("error not-exist exists");
+			field.closest("label").addClass("error");
+		}
+	});
+
+
+	
 	$("form").submit(function(e) {
 		var form = $(this);
 		var postform = true;
 		var fields = $(this).find("*[required]");;
-
-		// alright, go through the fields one by one
+		var details=[];
 		fields.each(function() {
 
 			var valid = validateField( $(this) );
-
-			// was the field validated?
 			if ( !valid ) {
-
-				// no!
 				postform = false;
+			}else{
+				details.push($(this).val().toLowerCase());
 			}
 		});
 
-		// so what's the result of the POST???
 		if ( !postform ) {
-
-			// if any field wasn't validated, stop the POST of the form
 			e.preventDefault();
-
 		} else {
-
-			// the validation was successful! But.. still stop the submit of the form and do it via AJAX
 			e.preventDefault();
-			nextSection();
+				if(ok!=1){
+				$.ajax({
+					url: "js/users.json",
+					dataType: "JSON"
+				})
+
+				.done(function( data ) {
+					var correct=0;
+					var i=0;
+					var found=false;
+
+					while (i<data.length && found==false){
+						if(details.length==4){
+							for(var k=0; k<details.length;k++){
+						
+								if(details[k] == data[i].firstname){
+									correct++;
+								}
+								if(details[k] == data[i].lastname){
+									correct++;
+								}
+								if(details[k] == data[i].profession){
+									correct++;
+								}
+								if(details[k] == data[i].svnumber){
+									correct++;
+								}
+							
+							}
+							if(correct==details.length){
+								found=true;
+							}else{
+								i++;
+								correct=0;
+							}
+						}else if (details.length==5){
+							for(var k=0; k<details.length;k++){
+							
+								if(details[k] == data[i].firstname){
+									correct++;
+								}
+								if(details[k] == data[i].lastname){
+									correct++;
+								}
+								if(details[k] == data[i].profession){
+									correct++;
+								}
+								if(details[k] == data[i].city){
+									correct++;
+								}
+
+								if(details[k] == data[i].workplace){
+									correct++;
+								}
+							
+							}
+
+							if(correct==details.length){
+								found=true;
+							}else{
+								i++;
+								correct=0;
+							}
+						}
+						
+					}
+					
+					if(found==true){
+						$('.next input').attr('required','required');
+						ok++;
+						nextSection();
+
+					}else{
+						alert(details);
+					}
+						
+				});
+			}
+
+		if(ok==1){
+			var fieldPass1=$(this).find("*[type=password]").first();
+			var fieldPass2=$(this).find("*[type=password]").last();
+			if(fieldPass1.val() == fieldPass2.val()){
+				nextSection();
+			}else{
+				fieldPass1.closest("label").addClass("error");
+				fieldPass2.closest("label").addClass("error");
+			}
+		}
+
 		}
 	});
 });
 
+
+
 function validateField( field ) {
 
-	// remove error messages from eventual earlier validation attempts
 	field.closest("label").removeClass("error");
 
-	// save/set some values
 	var type = field.attr("type");
 	var val = field.val();
 	var valid = true;
 
-	console.log(field);
-	// is it a <select>?
 	if ( field.is('select') ) {
 		val = field.find("option:selected").attr("value");
 		if (val == undefined || val == null || val == "") {
 			valid = false;
 		}
-
-	// is it a normal text field?
 	} else if ( type == "text" && (val == undefined || val == null || val == "") ) {
 		valid = false;
-
-	// is is an email field?
 	} else if ( type == "email" && !validateEmail(val) ) {
 		valid = false;
+	} else if ( type == "number" && (val == undefined || val == null || val == "") ) {
+		valid = false;
+	}else if ( type == "password" && (val == undefined || val == null || val == "") ) {
+		valid = false;
 	}
-
-	// was the field validated?
 	if ( !valid ) {
-		// no - add the error-appareance
 		field.closest("label").addClass("error");
 	}
-
 	return valid;
 }
 
 
-
-// function to check if email has correct syntax (using regex)
 function validateEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
